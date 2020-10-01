191215c170bea4462aa09d7a9d2e556c42a8827b
gaurav <gaurav.ag45@gmail.com>
2020-10-01 17:37:10 +0530

    Commit for variables tutorial

---
+++ harry.py
@@ -0,0 +1,5 @@
+import flask
+
+print('harry', 'video1', end = 'joker')
+print('video2')
+
---
+++ var.py
@@ -0,0 +1,24 @@
+var1 ='hello world'
+var2 = 4
+var3 = 36.7
+print(type(var1))
+print(var2 + var3)
+var4 = "54"
+var5 = "32"
+print(var4 + var5)
+print(int(var4) + int(var5))
+"""
+str()
+int()
+float()
+"""
+
+#print(10 * "HelloWorld\n")
+#print(10 * str(int(var4) + int(var5)))
+
+print("Enter")
+inp = input()
+
+print("your input:", inp)
+print(int(inp) + 10)
+
