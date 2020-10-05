511d7620513a78a9a0ae14cea12a51b8baaecb11
gaurav <gaurav.ag45@gmail.com>
2020-10-06 03:41:37 +0530

    Commit for oops tutorial

---
+++ foo.py
@@ -0,0 +1,12 @@
+def public_method():
+    print("I'm public")
+
+def _private_method():
+    print("I'm private")
+
+
+
+class Foo:
+
+    def __init__(self):
+        self.__bar = 42
---
+++ usefoo.py
@@ -0,0 +1,10 @@
+from foo import *
+public_method()
+# _foo_private_method()
+
+
+
+# from Foo import Foo
+foo = Foo()
+# print(foo.__bar)
+print(foo._Foo__bar)
